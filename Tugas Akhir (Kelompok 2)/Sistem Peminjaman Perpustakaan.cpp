81e5cb90b4cb44dcdf02b8b72dfb7d032d07abcf
Amru Muiz Fauzan <163871761+LordZerav@users.noreply.github.com>
2024-06-13 13:28:56 +0700

    Add files via upload

---
+++ Tugas Akhir (Kelompok 2)/Sistem Peminjaman Perpustakaan.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct 
+
+void
+
+int a;
+
+int main() {
+    cout << "::: SISTEM PEMINJAMAN PERPUSTAKAAN :::\n";
+    cout << "--------------------------------------\n";
+    
+    cout << "Masukkan Pilihan Anda : " << endl;
+    cin >> a;
+
+    cout << "[1] Input Data Buku \n";
+    cout << "[2] Daftar Buku\n";
+    cout << "[3] Input Data Peminjam\n";
+    cout << "[4] Daftar Peminjam\n";
+    
+}
